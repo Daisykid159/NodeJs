cd5872875cc457c671f03f6d605c0b4fe8a2d86f
Vũ Văn Dũng <daisyss159@gmail.com>
2022-07-04 20:26:32 +0700

    bài 9

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
 const port = 3000;
 
+app.use(morgan("combined"));
+
 app.get("/", (req, res) => {
-  res.send("I Just Love Linh <3 !");
+  res.send("I Just Love Linh <3");
 });
 
 app.listen(port, () => {
